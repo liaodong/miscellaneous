c9facf77eaea2babe8c42aa726a7389f4f670303
unkill <unkill@yeah.net>
2023-11-27 13:38:45 +0800

    Update 网速监控脚本.sh

--- 网速监控脚本.sh
+++ 网速监控脚本.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
- 
-ethn=$1
- 
+  
+# ethn=$1
+read -p "请输入网卡名：" ethn
+
 while true
 do
- RX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
- TX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
- sleep 1
- RX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
- TX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
- 
- clear
- echo -e "\t RX `date +%k:%M:%S` TX"
- 
- RX=$((${RX_next}-${RX_pre}))
- TX=$((${TX_next}-${TX_pre}))
- 
- if [[ $RX -lt 1024 ]];then
- RX="${RX}B/s"
- elif [[ $RX -gt 1048576 ]];then
- RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
- else
- RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
- fi
- 
- if [[ $TX -lt 1024 ]];then
- TX="${TX}B/s"
- elif [[ $TX -gt 1048576 ]];then
- TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
- else
- TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
- fi
- 
- echo -e "$ethn \t $RX $TX "
- 
+  RX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
+  TX_pre=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
+  sleep 1
+  RX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $2}')
+  TX_next=$(cat /proc/net/dev | grep $ethn | sed 's/:/ /g' | awk '{print $10}')
+
+  clear
+  echo -e "\t 接收 \t \t \t 发送 `date +%k:%M:%S`"
+
+  RX=$((${RX_next}-${RX_pre}))
+  TX=$((${TX_next}-${TX_pre}))
+
+  if [[ $RX -lt 1024 ]];then
+    RX="${RX}B/s"
+  elif [[ $RX -gt 1048576 ]];then
+    RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
+  else
+    RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
+  fi
+
+  if [[ $TX -lt 1024 ]];then
+    TX="${TX}B/s"
+  elif [[ $TX -gt 1048576 ]];then
+    TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
+  else
+    TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
+  fi
+
+  echo -e "$ethn \t $RX \t \t  $TX "
+
 done
+
+
